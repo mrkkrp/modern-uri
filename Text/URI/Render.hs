e23db4f53224318d6394d68a7b02c71336a511d9
mrkkrp <markkarpov92@gmail.com>
2017-10-26 18:50:01 +0700

    Make percent encoder a bit gentler

--- Text/URI/Render.hs
+++ Text/URI/Render.hs
@@ -52,8 +52,8 @@ render = TL.toStrict . TLB.toLazyText . render'
 -- | Render a given 'URI' value as a 'TLB.Builder'.
 
 render' :: URI -> TLB.Builder
-render' = genericRender TLB.decimal $
-  TLB.fromText . percentEncode . unRText
+render' = genericRender TLB.decimal $ \e ->
+  TLB.fromText . percentEncode e . unRText
 
 -- | Render a given 'URI.Normalized' value as a strict 'ByteString'.
 
@@ -63,13 +63,13 @@ renderBs = BL.toStrict . BLB.toLazyByteString . renderBs'
 -- | Render a given 'URI' value as a 'BLB.Builder'.
 
 renderBs' :: URI -> BLB.Builder
-renderBs' = genericRender BLB.wordDec $
-  BLB.byteString . TE.encodeUtf8 . percentEncode . unRText
+renderBs' = genericRender BLB.wordDec $ \e ->
+  BLB.byteString . TE.encodeUtf8 . percentEncode e . unRText
 
 ----------------------------------------------------------------------------
 -- Generic render
 
-type Render a b = (forall l. RText l -> b) -> a -> b
+type Render a b = (forall l. Bool -> RText l -> b) -> a -> b
 type R        b = (Monoid b, IsString b)
 
 genericRender :: R b => (Word -> b) -> Render URI b
@@ -84,23 +84,23 @@ rJust :: Monoid m => (a -> m) -> Maybe a -> m
 rJust = maybe mempty
 
 rScheme :: R b => Render (RText 'Scheme) b
-rScheme r = (<> ":") . r
+rScheme r = (<> ":") . r False
 
 rAuthority :: R b => (Word -> b) -> Render Authority b
 rAuthority d r Authority {..} = mconcat
   [ "//"
   , rJust (rUserInfo r) authUserInfo
-  , r authHost
+  , r False authHost
   , rJust ((":" <>) . d) authPort ]
 
 rUserInfo :: R b => Render UserInfo b
 rUserInfo r UserInfo {..} = mconcat
-  [ r uiUsername
-  , rJust ((":" <>) . r) uiPassword
+  [ r False uiUsername
+  , rJust ((":" <>) . r False) uiPassword
   , "@" ]
 
 rPath :: R b => Render [RText 'PathPiece] b
-rPath r ps = "/" <> mconcat (intersperse "/" (r <$> ps))
+rPath r ps = "/" <> mconcat (intersperse "/" (r True <$> ps))
 
 rQuery :: R b => Render [QueryParam] b
 rQuery r = \case
@@ -109,40 +109,43 @@ rQuery r = \case
 
 rQueryParam :: R b => Render QueryParam b
 rQueryParam r = \case
-  QueryFlag flag -> r flag
-  QueryParam k v -> r k <> "=" <> r v
+  QueryFlag flag -> r True flag
+  QueryParam k v -> r True k <> "=" <> r True v
 
 rFragment :: R b => Render (RText 'Fragment) b
-rFragment r = (<> "#") . r
+rFragment r = (<> "#") . r True
 
 ----------------------------------------------------------------------------
 -- Percent-encoding
 
 -- | Percent-encode a 'Text' value.
 
-percentEncode :: Text -> Text
-percentEncode txt = T.unfoldrN n f (bs, [])
+percentEncode
+  :: Bool              -- ^ Whether to leave @':'@ and @'@'@ unescaped
+  -> Text              -- ^ Input text to encode
+  -> Text              -- ^ Percent-encoded text
+percentEncode e txt = T.unfoldrN n f (bs, [])
   where
     f (bs', []) =
       case B.uncons bs' of
         Nothing -> Nothing
         Just (w, bs'') -> Just $
-          if isUnreserved w
+          if isUnreserved e w
             then (chr (fromIntegral w), (bs'', []))
             else let c:|cs = encodeByte w
                  in (c, (bs'', cs))
     f (bs', x:xs) = Just (x, (bs', xs))
     bs = TE.encodeUtf8 txt
     n  = B.foldl' (\n' w -> g w + n') 0 bs
-    g x = if isUnreserved x then 1 else 3
+    g x = if isUnreserved e x then 1 else 3
     encodeByte x = '%' :| [intToDigit h, intToDigit l]
       where
         (h, l) = fromIntegral x `quotRem` 16
 
 -- | The predicate selects unreserved bytes.
 
-isUnreserved :: Word8 -> Bool
-isUnreserved x
+isUnreserved :: Bool -> Word8 -> Bool
+isUnreserved t x
   | x >= 65 && x <= 90  = True -- 'A'..'Z'
   | x >= 97 && x <= 122 = True -- 'a'..'z'
   | x >= 48 && x <= 57  = True -- '0'..'9'
@@ -150,4 +153,6 @@ isUnreserved x
   | x == 95             = True -- '_'
   | x == 46             = True -- '.'
   | x == 126            = True -- '~'
+  | t && x == 58        = True -- ':'
+  | t && x == 64        = True -- '@'
   | otherwise           = False
