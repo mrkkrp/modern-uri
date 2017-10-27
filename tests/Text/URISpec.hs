d7e9f32a9e6429e2527e43c522eb5a73985653a4
mrkkrp <markkarpov92@gmail.com>
2017-10-28 01:48:23 +0700

    Rewrite the existing tests a bit

--- tests/Text/URISpec.hs
+++ tests/Text/URISpec.hs
@@ -1,5 +1,4 @@
 {-# LANGUAGE OverloadedStrings    #-}
-{-# LANGUAGE QuasiQuotes          #-}
 {-# OPTIONS_GHC -fno-warn-orphans #-}
 
 module Text.URISpec (spec) where
@@ -11,9 +10,8 @@ import Test.Hspec
 import Test.QuickCheck
 import Text.Megaparsec
 import Text.URI (URI (..), RTextException (..), RTextLabel (..))
-import qualified Data.Text   as T
-import qualified Text.URI    as URI
-import qualified Text.URI.QQ as QQ
+import qualified Data.Text as T
+import qualified Text.URI  as URI
 
 instance Arbitrary Text where
   arbitrary = T.pack <$> arbitrary
@@ -33,43 +31,43 @@ spec = do
           uriScheme (URI.makeAbsolute scheme uri) `shouldBe` Just scheme
   describe "mkScheme" $ do
     it "accepts valid schemes" $ do
-      URI.mkScheme "http"   `shouldReturn` [QQ.scheme|http|]
-      URI.mkScheme "HTTPS"  `shouldReturn` [QQ.scheme|https|]
-      URI.mkScheme "mailto" `shouldReturn` [QQ.scheme|mailto|]
-      URI.mkScheme "a+-."   `shouldReturn` [QQ.scheme|a+-.|]
+      URI.mkScheme "http"   `shouldRText` "http"
+      URI.mkScheme "HTTPS"  `shouldRText` "https"
+      URI.mkScheme "mailto" `shouldRText` "mailto"
+      URI.mkScheme "a+-."   `shouldRText` "a+-."
     it "rejects invalid schemes" $ do
       URI.mkScheme "123"   `shouldThrow` (== RTextException Scheme "123")
       URI.mkScheme "схема" `shouldThrow` (== RTextException Scheme "схема")
       URI.mkScheme "+-."   `shouldThrow` (== RTextException Scheme "+-.")
   describe "mkHost" $ do
     it "accepts valid IPv4 literals" $ do
-      URI.mkHost "127.0.0.1"    `shouldReturn` [QQ.host|127.0.0.1|]
-      URI.mkHost "198.98.43.23" `shouldReturn` [QQ.host|198.98.43.23|]
+      URI.mkHost "127.0.0.1"    `shouldRText` "127.0.0.1"
+      URI.mkHost "198.98.43.23" `shouldRText` "198.98.43.23"
     it "accepts valid IPv6 literals" $ do
-      URI.mkHost "[0123:4567:89ab:cdef:0:0:0:0]" `shouldReturn`
-        [QQ.host|[0123:4567:89ab:cdef:0:0:0:0]|]
-      URI.mkHost "[0123::4567:89ab]" `shouldReturn`
-        [QQ.host|[0123::4567:89ab]|]
-      URI.mkHost "[::0123:4567:89ab]" `shouldReturn`
-        [QQ.host|[::0123:4567:89ab]|]
-      URI.mkHost "[0123:4567:89ab::]" `shouldReturn`
-        [QQ.host|[0123:4567:89ab::]|]
+      URI.mkHost "[0123:4567:89ab:cdef:0:0:0:0]" `shouldRText`
+        "[0123:4567:89ab:cdef:0:0:0:0]"
+      URI.mkHost "[0123::4567:89ab]" `shouldRText`
+        "[0123::4567:89ab]"
+      URI.mkHost "[::0123:4567:89ab]" `shouldRText`
+        "[::0123:4567:89ab]"
+      URI.mkHost "[0123:4567:89ab::]" `shouldRText`
+        "[0123:4567:89ab::]"
     it "rejects invalid IPv6 literals" $ do
       URI.mkHost "[0123:4567:89ab]" `shouldThrow`
         (== RTextException Host "[0123:4567:89ab]")
       URI.mkHost "[0123::4567:89ab::]" `shouldThrow`
         (== RTextException Host "[0123::4567:89ab::]")
     it "accepts valid IP future literals" $ do
-      URI.mkHost "[va.something]" `shouldReturn` [QQ.host|[va.something]|]
-      URI.mkHost "[v1.123-456]"   `shouldReturn` [QQ.host|[v1.123-456]|]
+      URI.mkHost "[va.something]" `shouldRText` "[va.something]"
+      URI.mkHost "[v1.123-456]"   `shouldRText` "[v1.123-456]"
     it "rejects invalid IP future literals" $
       URI.mkHost "[vv.something]" `shouldThrow`
         (== RTextException Host "[vv.something]")
     it "accepts valid domain names" $ do
-      URI.mkHost "localhost"       `shouldReturn` [QQ.host|localhost|]
-      URI.mkHost "github.com"      `shouldReturn` [QQ.host|github.com|]
-      URI.mkHost "foo.example.com" `shouldReturn` [QQ.host|foo.example.com|]
-      URI.mkHost "юникод.рф"       `shouldReturn` [QQ.host|юникод.рф|]
+      URI.mkHost "LOCALHOST"       `shouldRText` "localhost"
+      URI.mkHost "github.com"      `shouldRText` "github.com"
+      URI.mkHost "foo.example.com" `shouldRText` "foo.example.com"
+      URI.mkHost "юникод.рф"       `shouldRText` "юникод.рф"
     it "rejects invalid hosts" $ do
       URI.mkHost "_something" `shouldThrow`
         (== RTextException Host "_something")
@@ -114,21 +112,35 @@ spec = do
   describe "parse and render" $
     it "parser and render are consistent" $
       property $ \uri ->
-        shouldParse urip (URI.render uri) uri
+        shouldParse (URI.render uri) uri
+  -- TODO something nominal for rendering in text
+  -- TODO rendering of byte strings
+  -- TODO parser: various stuff, check corner cases
+  -- TODO parser: check parse errors
 
 ----------------------------------------------------------------------------
 -- Helpers
 
-urip :: Parsec Void Text URI
-urip = URI.parse
+-- | Expect that the given action constructs 'URI.RText' with certain text
+-- inside.
 
-shouldParse :: (Eq a, Show a)
-  => Parsec Void Text a
-  -> Text
-  -> a
+shouldRText
+  :: IO (URI.RText l)  -- ^ Action that produces refined text
+  -> Text              -- ^ Inner text to compare with
   -> Expectation
-shouldParse p s a =
-  case runParser p "" s of
+shouldRText rtext txt = do
+  txt' <- rtext
+  URI.unRText txt' `shouldBe` txt
+
+-- | Expect that the specified input for parser will produce 'URI' equal to
+-- a given one.
+
+shouldParse
+  :: Text              -- ^ Parser input
+  -> URI               -- ^ 'URI' to compare with
+  -> Expectation
+shouldParse s a =
+  case runParser (URI.parse :: Parsec Void Text URI) "" s of
     Left e -> expectationFailure $
       "the parser is expected to succeed, but it failed with:\n" ++
       parseErrorPretty' s e
