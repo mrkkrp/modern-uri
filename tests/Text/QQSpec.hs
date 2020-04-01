db2d0fe74d865885058038bf446c34445f17333b
Chris Martin <ch.martin@gmail.com>
2020-04-01 15:59:20 -0600

    Implement ‘quotePat’ for all quasi-quoters
    This adds a very simple implementation of ‘quotePat’ for all of the
    quasi-quoters. It just constructs a view pattern that tests for equality
    with the given value using (==).
    
    Note that I also modified the existing test cases for ‘queryValue’ and
    ‘fragment’, which I believe were erroneously using the ‘mkQueryKey’
    function.

--- tests/Text/QQSpec.hs
+++ tests/Text/QQSpec.hs
@@ -1,6 +1,8 @@
+{-# LANGUAGE LambdaCase        #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE QuasiQuotes       #-}
 {-# LANGUAGE TemplateHaskell   #-}
+{-# LANGUAGE ViewPatterns      #-}
 
 module Text.QQSpec (spec) where
 
@@ -10,39 +12,110 @@ import qualified Text.URI.QQ as QQ
 
 spec :: Spec
 spec = do
-  describe "uri" . it "works" $ do
-    let uriQQ = [QQ.uri|https://markkarpov.com|]
-    uri <- mkURI "https://markkarpov.com"
-    uriQQ `shouldBe` uri
-  describe "scheme" . it "works" $ do
-    let schemeQQ = [QQ.scheme|https|]
-    scheme <- mkScheme "https"
-    schemeQQ `shouldBe` scheme
-  describe "host" . it "works" $ do
-    let hostQQ = [QQ.host|markkarpov.com|]
-    host <- mkHost "markkarpov.com"
-    hostQQ `shouldBe` host
-  describe "username" . it "works" $ do
-    let usernameQQ = [QQ.username|mark|]
-    username <- mkUsername "mark"
-    usernameQQ `shouldBe` username
-  describe "password" . it "works" $ do
-    let passwordQQ = [QQ.password|secret123|]
-    password <- mkPassword "secret123"
-    passwordQQ `shouldBe` password
-  describe "pathPiece" . it "works" $ do
-    let pathPieceQQ = [QQ.pathPiece|foo|]
-    pathPiece <- mkPathPiece "foo"
-    pathPieceQQ `shouldBe` pathPiece
-  describe "queryKey" . it "works" $ do
-    let queryKeyQQ = [QQ.queryKey|foo|]
-    queryKey <- mkQueryKey "foo"
-    queryKeyQQ `shouldBe` queryKey
-  describe "queryValue" . it "works" $ do
-    let queryValueQQ = [QQ.queryValue|bar|]
-    queryValue <- mkQueryKey "bar"
-    queryValueQQ `shouldBe` queryValue
-  describe "fragment" . it "works" $ do
-    let fragmentQQ = [QQ.fragment|frag|]
-    fragment <- mkQueryKey "frag"
-    fragmentQQ `shouldBe` fragment
+
+  describe "uri" $ do
+    it "works as an expression" $ do
+      let uriQQ = [QQ.uri|https://markkarpov.com|]
+      uri <- mkURI "https://markkarpov.com"
+      uriQQ `shouldBe` uri
+    it "works as a pattern" $
+      mkURI "https://markkarpov.com" >>= \case
+        [QQ.uri|https://haskell.org|] -> shouldNotMatch
+        [QQ.uri|https://markkarpov.com|] -> return ()
+        _ -> shouldHaveMatchedAlready
+
+  describe "scheme" $ do
+    it "works as an expression" $ do
+      let schemeQQ = [QQ.scheme|https|]
+      scheme <- mkScheme "https"
+      schemeQQ `shouldBe` scheme
+    it "works as a pattern" $
+      mkScheme "https" >>= \case
+        [QQ.scheme|ftp|] -> shouldNotMatch
+        [QQ.scheme|https|] -> return ()
+        _ -> shouldHaveMatchedAlready
+
+  describe "host" $ do
+    it "works as an expression" $ do
+      let hostQQ = [QQ.host|markkarpov.com|]
+      host <- mkHost "markkarpov.com"
+      hostQQ `shouldBe` host
+    it "works as a pattern" $ do
+      mkHost "markkarpov.com" >>= \case
+        [QQ.host|haskell.org|] -> shouldNotMatch
+        [QQ.host|markkarpov.com|] -> return ()
+        _ -> shouldHaveMatchedAlready
+
+  describe "username" $ do
+    it "works as an expression" $ do
+      let usernameQQ = [QQ.username|mark|]
+      username <- mkUsername "mark"
+      usernameQQ `shouldBe` username
+    it "works as a pattern" $ do
+      mkUsername "mark" >>= \case
+        [QQ.username|chris|] -> shouldNotMatch
+        [QQ.username|mark|] -> return ()
+        _ -> shouldHaveMatchedAlready
+
+  describe "password" $ do
+    it "works as an expression" $ do
+      let passwordQQ = [QQ.password|secret123|]
+      password <- mkPassword "secret123"
+      passwordQQ `shouldBe` password
+    it "works as a pattern" $ do
+      mkPassword "secret123" >>= \case
+        [QQ.password|secretXYZ|] -> shouldNotMatch
+        [QQ.password|secret123|] -> return ()
+        _ -> shouldHaveMatchedAlready
+
+  describe "pathPiece" $ do
+    it "works as an expression" $ do
+      let pathPieceQQ = [QQ.pathPiece|foo|]
+      pathPiece <- mkPathPiece "foo"
+      pathPieceQQ `shouldBe` pathPiece
+    it "works as a pattern" $ do
+      mkPathPiece "foo" >>= \case
+        [QQ.pathPiece|bar|] -> shouldNotMatch
+        [QQ.pathPiece|foo|] -> return ()
+        _ -> shouldHaveMatchedAlready
+
+  describe "queryKey" $ do
+    it "works as an expression" $ do
+      let queryKeyQQ = [QQ.queryKey|foo|]
+      queryKey <- mkQueryKey "foo"
+      queryKeyQQ `shouldBe` queryKey
+    it "works as a pattern" $ do
+      mkQueryKey "foo" >>= \case
+        [QQ.queryKey|xyz|] -> shouldNotMatch
+        [QQ.queryKey|foo|] -> return ()
+        _ -> shouldHaveMatchedAlready
+
+  describe "queryValue" $ do
+    it "works as an expression" $ do
+      let queryValueQQ = [QQ.queryValue|bar|]
+      queryValue <- mkQueryValue "bar"
+      queryValueQQ `shouldBe` queryValue
+    it "works as a pattern" $ do
+      mkQueryValue "bar" >>= \case
+        [QQ.queryValue|xyz|] -> shouldNotMatch
+        [QQ.queryValue|bar|] -> return ()
+        _ -> shouldHaveMatchedAlready
+
+  describe "fragment" $ do
+    it "works as an expression" $ do
+      let fragmentQQ = [QQ.fragment|frag|]
+      fragment <- mkFragment "frag"
+      fragmentQQ `shouldBe` fragment
+    it "works as a pattern" $ do
+      mkFragment "frag" >>= \case
+        [QQ.fragment|xyz|] -> shouldNotMatch
+        [QQ.fragment|frag|] -> return ()
+        _ -> shouldHaveMatchedAlready
+
+shouldNotMatch :: Expectation
+shouldNotMatch = expectationFailure
+  "First case should not have matched, but did"
+
+shouldHaveMatchedAlready :: Expectation
+shouldHaveMatchedAlready = expectationFailure
+  "Second case should have matched, but didn't"
